f91fd0bc3f0dc185e288283f8d21433ce365d88b
amardeep <amardeepsit@gmail.com>
2020-04-09 20:23:05 +0530

    Added runtime permission

--- app/src/main/java/com/camerax/CameraActivity.kt
+++ app/src/main/java/com/camerax/CameraActivity.kt
@@ -1,6 +1,9 @@
 package com.camerax
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
@@ -10,13 +13,20 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
 
-class CameraActivity: AppCompatActivity() {
+class CameraActivity : AppCompatActivity(), LifecycleOwner {
 
+    private val CODE = 10
+    private val PERMISSION = arrayOf(Manifest.permission.CAMERA)
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var previewView: PreviewView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        checkAllPermission()
+    }
+
+    private fun setupPreview() {
         previewView = findViewById<PreviewView>(R.id.preview_view)
 
         cameraProviderFuture = ProcessCameraProvider.getInstance(this)
@@ -27,15 +37,45 @@ class CameraActivity: AppCompatActivity() {
         }, ContextCompat.getMainExecutor(this))
     }
 
-    fun bindPreview(cameraProvider : ProcessCameraProvider) {
-        var preview : Preview = Preview.Builder()
+    private fun checkAllPermission() {
+        if (allPermissionGranted()) {
+            setupPreview()
+        } else {
+            requestPermissions(PERMISSION, CODE)
+        }
+    }
+
+    private fun allPermissionGranted() =
+        PERMISSION.all { checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CODE) {
+            if (allPermissionGranted()) {
+                setupPreview()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        var preview: Preview = Preview.Builder()
             .build()
 
-        var cameraSelector : CameraSelector = CameraSelector.Builder()
+        var cameraSelector: CameraSelector = CameraSelector.Builder()
             .requireLensFacing(CameraSelector.LENS_FACING_BACK)
             .build()
 
-        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+        var camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview)
 
 
         preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.cameraInfo))
